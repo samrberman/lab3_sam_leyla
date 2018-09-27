0b256e4a4db20b70a72e7f06bb034706bcffe7af
Samantha Berman <samrberman@gmail.com>
2018-09-27 14:44:15 -0400

    end of class

--- readability.js
+++ readability.js
@@ -12,8 +12,27 @@ function readability(filename, callback) {
 
         // TODO: parse and analyze the file contents
 
-        callback({test: "Replace this object with more information!"});
-    });
+        let sentence_count = tokenizeEnglish.sentences()(contents).length;
+        let char_count = tokenize.characters()(contents).length;
+        let word_count = tokenize.words()(contents).length;
+
+        let extract_letters = tokenize.re(/[A-Za-z]/);
+        let letter_count = extract_letters(contents).length;
+        let extract_numbers = tokenize.re(/[0-9]/);
+        let number_count = extract_numbers(contents).length;
+
+        let CL_value = colemanLiau(letter_count, word_count, sentence_count);
+        let ARI_value = automatedReadabilityIndex(letter_count, number_count, word_count, sentence_count);
+
+        callback(sentence_count + " sentences\n" +
+                 char_count + " characters\n" +
+                 word_count + " words\n" +
+                 letter_count + " letters\n" +
+                 number_count + " numbers\n" +
+                 "\n" +
+                 "Coleman Liau: " + CL_value +
+                 "\nAutomated Readability Index: " + ARI_value + "\n")
+        });
 }
 
 // Computes Coleman-Liau readability index
@@ -32,7 +51,7 @@ function automatedReadabilityIndex(letters, numbers, words, sentences) {
 
 // Calls the readability function on the provided file and defines callback behavior
 if (process.argv.length >= 3) {
-    readability(process.argv[2], data => {
+    readability("texts/"+process.argv[2], data => {
         console.log(data);
     });
 }
