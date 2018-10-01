defc9382693a710b4689d0fbfbd0d88beb002ba4
Samantha Berman <samrberman@gmail.com>
2018-09-30 20:57:49 -0400

    stuck at the end of sql part

--- readability.js
+++ readability.js
@@ -10,30 +10,78 @@ function readability(filename, callback) {
     fs.readFile(filename, "utf8", (err, contents) => {
         if (err) throw err;
 
-        // TODO: parse and analyze the file contents
 
-        let sentence_count = tokenizeEnglish.sentences()(contents).length;
-        let char_count = tokenize.characters()(contents).length;
-        let word_count = tokenize.words()(contents).length;
+        let title = process.argv[2].split('.')[0];
 
-        let extract_letters = tokenize.re(/[A-Za-z]/);
-        let letter_count = extract_letters(contents).length;
-        let extract_numbers = tokenize.re(/[0-9]/);
-        let number_count = extract_numbers(contents).length;
+        let db = new sqlite3.Database('./texts.db', sqlite3.OPEN_READWRITE, (err) => {
+          if (err) {
+              console.error(err.message);
+            };
+  //      db.get(`SELECT text_id FROM text_info WHERE title=${title}`, (err, row) => {
+  //        if (err) {
+//            return console.error(err.message)
+//          }
+          db.get(`SELECT EXISTS(SELECT 1 FROM text_info WHERE title=${title})`, (err, row) => {
+            if (err) {
+              return console.error(err.message);
+            }
+            console.log(row);
+          });
+          if (true) {
+            console.log('something in the row')
+            db.get(`SELECT * FROM text_info WHERE title=${title}`, (err, row) => {
+                  if (err) {
+                    return console.error(err.message);
+                  }
+                  let sentence_count = row.sentence_count;
+                  let char_count = row.char_count;
+                  let word_count = row.word_count;
+                  let letter_count = row.letter_count;
+                  let number_count = row.number_count;
+                  let CL_value = row.CL_value;
+                  let ARI_value = row.ARI_value;
 
-        let CL_value = colemanLiau(letter_count, word_count, sentence_count);
-        let ARI_value = automatedReadabilityIndex(letter_count, number_count, word_count, sentence_count);
+                  callback(sentence_count + " sentences\n" +
+                           char_count + " characters\n" +
+                           word_count + " words\n" +
+                           letter_count + " letters\n" +
+                           number_count + " numbers\n" +
+                           "\nColeman Liau: " + CL_value +
+                           "\nAutomated Readability Index: " + ARI_value + "\n")
+                });
+              }
+          else {
+            console.log('nothing in the row')
+            let sentence_count = tokenizeEnglish.sentences()(contents).length;
+            let char_count = tokenize.characters()(contents).length;
+            let word_count = tokenize.words()(contents).length;
 
-        callback(sentence_count + " sentences\n" +
-                 char_count + " characters\n" +
-                 word_count + " words\n" +
-                 letter_count + " letters\n" +
-                 number_count + " numbers\n" +
-                 "\n" +
-                 "Coleman Liau: " + CL_value +
-                 "\nAutomated Readability Index: " + ARI_value + "\n")
-        });
-}
+            let extract_letters = tokenize.re(/[A-Za-z]/);
+            let letter_count = extract_letters(contents).length;
+            let extract_numbers = tokenize.re(/[0-9]/);
+            let number_count = extract_numbers(contents).length;
+
+            let CL_value = colemanLiau(letter_count, word_count, sentence_count);
+            let ARI_value = automatedReadabilityIndex(letter_count, number_count, word_count, sentence_count);
+
+            store_info(db, sentence_count, char_count, word_count, letter_count,
+                       number_count, CL_value, ARI_value, title);
+
+                       callback(sentence_count + " sentences\n" +
+                                char_count + " characters\n" +
+                                word_count + " words\n" +
+                                letter_count + " letters\n" +
+                                number_count + " numbers\n" +
+                                "\nColeman Liau: " + CL_value +
+                                "\nAutomated Readability Index: " + ARI_value + "\n")
+            }
+            db.close();
+
+          })
+
+
+        })};
+
 
 // Computes Coleman-Liau readability index
 function colemanLiau(letters, words, sentences) {
@@ -57,4 +105,16 @@ if (process.argv.length >= 3) {
 }
 else {
     console.log("Usage: node readability.js <file>");
+}
+
+//function read_info (db, title) {}
+
+function store_info (db, sentences, characters, words, letters, numbers, CL_value, ARI_value, title) {
+      db.run(`INSERT INTO text_info(sentence_count, char_count, word_count, letter_count, number_count, CL_value, ARI_value, title) VALUES(?, ?, ?, ?, ?, ?, ?, ?)`,
+              [sentences, characters, words, letters, numbers, CL_value, ARI_value, title], function(err) {
+        if (err) {
+          return console.log(err.message);
+        }
+        console.log('row inserted');
+      });
 }
